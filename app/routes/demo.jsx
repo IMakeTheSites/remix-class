91f78b077e943888cf3e0724caee0850fecf3361
Mark Wlodawski <imakethesites@gmail.com>
2023-02-13 14:21:29 -0500

    Adding components and styles for Notes app, surfacing component styles

--- app/routes/demo.jsx
+++
@@ -1,6 +0,0 @@
-import { Link } from "@remix-run/react";
-
-export default function DemoPage() {
-  return <h1>Demo Page</h1>;
-  <Link to="index">Home</Link>;
-}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,18 @@
+import { Link } from "@remix-run/react";
+import homestyles from "~/styles/home.css";
+
 export default function Index() {
-  return <h1>Hello World</h1>;
+  return (
+    <main id="content">
+      <h1>Keep Track of Your Notes</h1>
+      <p>Try our early beta and never lose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p>
+    </main>
+  );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: homestyles }];
 }
