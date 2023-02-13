5bfead3cf19d3ebf98a20abeecc4887806d196f2
Mark Wlodawski <imakethesites@gmail.com>
2023-02-13 15:41:37 -0500

    Returning and using fetched data

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,18 +1,25 @@
 import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
-import NoteList, { link as noteListLinks } from "~/components/NoteList";
+import NoteList, { links as noteListLinks } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
+  const notes = useLoaderData();
+
   return (
     <main>
       <NewNote />
-      <NoteList />
+      <NoteList notes={notes} />
     </main>
   );
 }
 
-export function loader() {}
+export async function loader() {
+  const notes = await getStoredNotes();
+  return notes;
+  // return json(notes)
+}
 
 export async function action({ request }) {
   const formData = await request.formData();
