53873eec3d1290615e3f14139aff4c491ef2917f
Mark Wlodawski <imakethesites@gmail.com>
2023-02-13 15:02:09 -0500

    Adding backend code, redirecting, and fetching page data via loader()

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,13 +1,28 @@
+import { redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import NoteList, { link as noteListLinks } from "~/components/NoteList";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   return (
     <main>
       <NewNote />
+      <NoteList />
     </main>
   );
 }
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect("/notes");
+}
+
 export function links() {
-  return [...newNoteLinks()];
+  return [...newNoteLinks(), ...noteListLinks()];
 }
