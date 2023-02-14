f83cf7dcb8488e5e9a0d057c38ed12a5210f3a6c
Mark Wlodawski <imakethesites@gmail.com>
2023-02-14 08:49:02 -0500

    Validating input and returning data in actions

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -25,6 +25,10 @@ export async function action({ request }) {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData);
 
+  if (noteData.title.trim().length < 5) {
+    return { message: "Invalid title - must be at least 5 characters long." };
+  }
+
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
   const updatedNotes = existingNotes.concat(noteData);
